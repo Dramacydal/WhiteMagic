2d725946cbd73b0657242e4145f48330fbe812f9
Yaki Khadafi <ElSolDolLo@gmail.com>
2014-11-25 00:55:01 +0300

    some intptr extensions

--- WhiteMagic/MagicExtensions.cs
+++ WhiteMagic/MagicExtensions.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Linq;
 using WhiteMagic.Modules;
 
 namespace WhiteMagic
 {
-    public static class Extensions
+    public static class MagicExtensions
     {
         public static uint Call(this ProcessDebugger pd, ModulePointer offs, CallingConventionEx cv, params object[] args)
         {
@@ -28,23 +29,45 @@ namespace WhiteMagic
         /// <returns></returns>
         public static string AsHexString(this byte[] array, bool reverse = false)
         {
-            var ret = string.Empty;
             if (array.Length == 0)
-                return ret;
+                return string.Empty;
 
-            if (reverse)
-                for (var i = array.Length - 1; i >= 0; --i)
-                    ret += string.Format("{0:X} ", array[i]);
-            else
-                for (var i = 0; i < array.Length; ++i)
-                    ret += string.Format("{0:X} ", array[i]);
-
-            return ret;
+            return string.Concat((reverse ? array.Reverse() : array).Select(e => string.Format("{0:X2} ", e)));
         }
 
         public static bool IsValid(this IntPtr p)
         {
             return p != new IntPtr(int.MaxValue);
+        }
+
+        public static uint ToUInt32(this IntPtr p)
+        {
+            return (uint)p.ToInt32();
+        }
+
+        public static IntPtr Add(this IntPtr pointer, int offset)
+        {
+            return IntPtr.Add(pointer, offset);
+        }
+
+        public static IntPtr Add(this IntPtr pointer, uint offset)
+        {
+            return IntPtr.Add(pointer, (int)offset);
+        }
+
+        public static IntPtr Add(this IntPtr pointer, IntPtr pointer2)
+        {
+            return IntPtr.Add(pointer, pointer2.ToInt32());
+        }
+
+        public static IntPtr Subtract(this IntPtr pointer, int offset)
+        {
+            return IntPtr.Subtract(pointer, offset);
+        }
+
+        public static IntPtr Subtract(this IntPtr pointer, IntPtr pointer2)
+        {
+            return IntPtr.Subtract(pointer, pointer2.ToInt32());
         }
     }
 }
